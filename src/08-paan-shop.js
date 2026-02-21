8c281b50ec61bd7127d7170138397bb575f61864
Moqtadir <abdulmoqtadir1@gmail.com>
2026-02-21 05:38:21 +0530

    task 08 done

--- src/08-paan-shop.js
+++ src/08-paan-shop.js
@@ -45,18 +45,40 @@
  *   createPaanOrder({type:"meetha"}, {extra:"gulkand"}) // => {type:"meetha",extra:"gulkand"}
  *   updatePrices({meetha:30, saada:20}, 10)              // => {meetha:40, saada:30}
  */
+
 export function createPaanOrder(basePaan, customizations) {
   // Your code here
+  if (typeof basePaan !== "object" || basePaan === null) return {};
+  if (typeof customizations !== "object") return Object.assign({}, basePaan);
+
+  return Object.assign({}, basePaan, customizations);
 }
 
 export function freezeMenu(menu) {
   // Your code here
+  if (typeof menu !== "object" || menu === null) return {};
+  return Object.freeze(menu);
 }
 
 export function updatePrices(menu, increase) {
   // Your code here
+  if (typeof menu !== "object" || menu === null || typeof increase !== "number")
+    return {};
+  // const newMenu = Object.entries(menu);
+  return Object.fromEntries(
+    Object.entries(menu).map(([key, val]) => [key, val + increase]),
+  );
 }
 
 export function mergeDailySpecials(regularMenu, specialsMenu) {
+  if (typeof regularMenu !== "object" || typeof specialsMenu !== "object")
+    return {};
+  if (regularMenu === null) {
+    regularMenu = {};
+  }
+  if (specialsMenu === null) {
+    specialsMenu = {};
+  }
+  return { ...regularMenu, ...specialsMenu };
   // Your code here
 }
